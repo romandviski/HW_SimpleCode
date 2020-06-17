a68357e8b988860e2ff290977010abc92d9a9347
Mikhail <romandviski@mail.ru>
2020-06-18 01:21:13 +0300

    Домашка 2
    ДЗ. Напишите программу, проверяющую число, введенное с клавиатуры на четность.

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,3 @@
-// Hello, world!!!
 #include <iostream>
 #include <string>
 
@@ -6,26 +5,30 @@ using namespace std;
 
 int main()
 {
+	// ДЗ. Напишите программу, проверяющую число, введенное с клавиатуры на четность. 
 	setlocale(LC_ALL, "ru");
 
-	string Stroka = "Русишь?";
-	int Cifra1 = 0, Cifra2 = 0, Cifra3 = 0;
-	int Summa, Proizv;
-	float Srednee;
+	int Cifra;
 
-	cout << "Введи 3 числа: ";
-	cout << endl;
-	cin >> Cifra1 >> Cifra2 >> Cifra3;
+	cout << "Введите число" << endl;
+	cin >> Cifra;
 	cout << endl;
 
-	Summa = Cifra1 + Cifra2 + Cifra3;
-	Proizv = Cifra1 * Cifra2 * Cifra3;
-	Srednee = (float)Summa / 3;
+	if (Cifra % 2 == 0)
+	{
+		cout << "Ваше число " << Cifra << ". Оно чётное." << endl;
+	}
+	else if (Cifra % 2 != 0)
+	{
+		cout << "Ваше число " << Cifra << ". Оно нечётное." << endl;
+	}
+	else // дебаг =)
+	{
+		cout << "Что-то пошло не так" << endl;
+	}
 
 
-	cout << "Сумма: " << Summa << endl;
-	cout << "Производная: " << Proizv << endl;
-	cout << "Среднее: " << Srednee << endl;
+
 	
 
 
