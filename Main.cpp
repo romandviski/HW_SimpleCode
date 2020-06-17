2647c4d9a7eac448bf468ec33ce03fff9cd84253
Mikhail <romandviski@mail.ru>
2020-06-18 00:35:47 +0300

    Домашка 1
    Введите три числа и выведите на экран значение суммы, произведения и среднее арифметическое этих чисел.

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,41 @@
-// Hello, world.
+// Hello, world!!!
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	string Stroka = "Русишь?";
+	int Cifra1 = 0, Cifra2 = 0, Cifra3 = 0;
+	int Summa, Proizv, Srednee;
+
+	cout << "Введи 3 числа: ";
+	cout << endl;
+	cin >> Cifra1 >> Cifra2 >> Cifra3;
+	cout << endl;
+
+	Summa = Cifra1 + Cifra2 + Cifra3;
+	Proizv = Cifra1 * Cifra2 * Cifra3;
+	Srednee = Summa / 3;
+
+
+	cout << "Сумма: " << Summa << endl;
+	cout << "Производная: " << Proizv << endl;
+	cout << "Среднее: " << Srednee << endl;
+	
+
+
+
+
+
+
+
+
+
+
+	cout << endl;
+	//system("pause"); // что бы не закрывалась консоль
+}
