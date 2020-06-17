08745908b81ee444a21bf32976e135ee07f91f07
Mikhail <romandviski@mail.ru>
2020-06-18 02:45:10 +0300

    Немного переписал

--- Main.cpp
+++ Main.cpp
@@ -12,26 +12,33 @@ int main()
 */
 	setlocale(LC_ALL, "ru");
 
-	int Cifra1, Cifra2;
-	int var;
+	float Cifra1, Cifra2;
+	int var = 6;
 	
 	cout << "Добро пожаловать в программу \"Бомжатский калькулятор\"" << endl << endl;
 	cout << "Введите 1 число" << endl;
 	cin >> Cifra1;
 	cout << endl;
-	cout << 
+	cout <<
 		"Выберите номер действия: " << endl <<
-		"1. Сложение"  << endl <<
+		"1. Сложение" << endl <<
 		"2. Вычитание" << endl <<
 		"3. Умножение" << endl <<
-		"4. Деление"   << endl;
+		"4. Деление" << endl;
+		//"или введите его с клавиатуры (+ - * /)" <<endl;
+
 	cout << endl;
-	cout << "Ваш выбор: ";
-	cin >> var;
+	
+		while (var > 4)
+		{ 
+			cout << "Ваш выбор: ";
+			cin >> var;
+		}
+
 	cout << endl;
 	cout << "Введите 2 число" << endl;
 	cin >> Cifra2;
-	
+
 	switch (var)
 	{
 	case 1:
@@ -48,14 +55,18 @@ int main()
 		break;
 	case 4:
 		cout << endl;
+		if (Cifra2 != 0)
 		cout << "Ответ будет: " << (float)Cifra1 / Cifra2 << endl;
+		else
+		cout << "Эх... На 0 делить нельзя!" << endl;
 		break;
 	default:
 		cout << endl;
-		cout << "Ваше число: " << var << ". Оно за гранью понимания =) "<< endl;
+		cout << "Ваше выбор: " << var << ". Он за гранью понимания =) "<< endl;
 		break;
 	}
 
 	cout << endl;
+	//system("CLS"); // очистить консоль
 	//system("pause"); // что бы не закрывалась консоль
 }
