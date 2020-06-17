fa95af66ec39029e817ec6e7d4ede3d8d272307c
Mikhail <romandviski@mail.ru>
2020-06-18 02:04:04 +0300

    Цикл while (i < 10)
    	while (i < 10)
    	{
    		cout << "Строка №" << i+1 << endl;
    		i++;
    	}

--- Main.cpp
+++ Main.cpp
@@ -12,48 +12,19 @@ int main()
 */
 	setlocale(LC_ALL, "ru");
 
-	int Cifra1, Cifra2;
+	int Cifra1;
 	int var;
+	int i = 0;
 	
-	cout << "Добро пожаловать в программу \"Бомжатский калькулятор\"" << endl << endl;
-	cout << "Введите 1 число" << endl;
+	//cout << "Добро пожаловать в программу \"Бомжатский калькулятор\"" << endl << endl;
+	cout << "Сколько строк?" << endl;
 	cin >> Cifra1;
 	cout << endl;
-	cout << 
-		"Выберите номер действия: " << endl <<
-		"1. Сложение"  << endl <<
-		"2. Вычитание" << endl <<
-		"3. Умножение" << endl <<
-		"4. Деление"   << endl;
-	cout << endl;
-	cout << "Ваш выбор: ";
-	cin >> var;
-	cout << endl;
-	cout << "Введите 2 число" << endl;
-	cin >> Cifra2;
-	
-	switch (var)
+
+	while (i < Cifra1)
 	{
-	case 1:
-		cout << endl;
-		cout << "Ответ будет: " << Cifra1 + Cifra2 << endl;
-		break;
-	case 2:
-		cout << endl;
-		cout << "Ответ будет: " << Cifra1 - Cifra2 << endl;
-		break;
-	case 3:
-		cout << endl;
-		cout << "Ответ будет: " << Cifra1 * Cifra2 << endl;
-		break;
-	case 4:
-		cout << endl;
-		cout << "Ответ будет: " << (float)Cifra1 / Cifra2 << endl;
-		break;
-	default:
-		cout << endl;
-		cout << "Ваше число: " << var << ". Оно за гранью понимания =) "<< endl;
-		break;
+		cout << "Строка №" << i+1 << endl;
+		i++;
 	}
 
 	cout << endl;
