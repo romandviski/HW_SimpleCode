e6ec2cf02efd67eba1fc1d43b04116b3814e3577
Mikhail <romandviski@mail.ru>
2020-06-19 01:35:43 +0300

    Домашка 5
    Написать программу, которая находит сумму всех целых нечетных чисел в диапазоне, указанном пользователе.

--- Main.cpp
+++ Main.cpp
@@ -3,54 +3,76 @@
 
 using namespace std;
 
+void GetMegaEven(int A, int N, int C)
+{
+	int Otvet = 0;
+	if (A < N) {
+		for (int i = A; i <= N; i++)
+		{
+			if (i % 2 == 0 && C == 2)
+			{
+				cout << i << endl;
+				Otvet += i;
+			}
+			else if (i % 2 == 1 && C == 1)
+			{
+				cout << i << endl;
+				Otvet += i;
+			}
+		}
+	}
+	else {
+		for (int i = N; i <= A; i++)
+		{
+			if (i % 2 == 0 && C == 2)
+			{
+				cout << i << endl;
+				Otvet += i;
+			}
+			else if (abs(i % 2) == 1 && C == 1)
+			{
+				cout << i << endl;
+				Otvet += i;
+			}
+		}
+	}
+
+	cout << "Сумма всех чисел будет = " << Otvet << endl;
+
+}
+
 int main()
 {
 /* 
 	ДЗ.
-	1. Разработать программу, которая выводит на экран линию из символов. 
-	Число символов, какой использовать символ, и какая будет линия - вертикальная, или горизонтальная - указывает пользователь.
-	2. Написать программу, которая находит сумму всех целых нечетных чисел в диапазоне, указанном пользователе.
+	Написать программу, которая находит сумму всех целых нечетных чисел в диапазоне, указанном пользователе.
 */
 	setlocale(LC_ALL, "ru");
 
-	int Cifra1;
-	char Simvol;
-	int var;
-	int index = 0;
-	
-	cout << "Какой символ использовать?" << endl;
-	cin >> Simvol;
+	int Nachalo;
+	int Konec;
+	int Vibor = 5;
+
+	cout << "Введите начало" << endl;
+	cin >> Nachalo;
 	system("CLS");
 
-	cout << "Сколько символов?" << endl;
-	cin >> Cifra1;
+	cout << "Введите конец" << endl;
+	cin >> Konec;
 	system("CLS");
 
-	cout << "1 если вертикально, 2 если горизонтально =)" << endl;
-	cin >> var;
-	system("CLS");
-
-	switch (var)
+	while (Vibor > 2 || Vibor < 1)
 	{
-	case 1:
-		while (index < Cifra1)
-		{
-			cout << Simvol << endl;
-			index++;
-		}
-		break;
-	case 2:
-		while (index < Cifra1)
-		{
-			cout << Simvol;
-			index++;
-		}
-		break;
-	default:
+		cout << "1 если не четные, 2 если четные" << endl;
+		cin >> Vibor;
 		system("CLS");
-		cout << "Неверный выбор" << endl;
-		break;
+		cout << "Ты ввел " << Vibor << ". Это не верно! " << endl;
 	}
+
+	system("CLS");
+
+	GetMegaEven(Nachalo, Konec, Vibor);
+
 
 	//system("CLS");
 	//system("pause"); // что бы не закрывалась консоль
